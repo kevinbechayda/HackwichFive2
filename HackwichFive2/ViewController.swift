1a989ca0ffb7e0f3226c2ee0332d28c323994f20
Kevin Bechayda <kevinbechayda@gmail.com>
2018-03-19 10:49:48 -1000

    End of class

--- HackwichFive2/ViewController.swift
+++ HackwichFive2/ViewController.swift
@@ -10,16 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var kapoleiRestaurantsArray = ["Kapolei Kalawai"]
+    
+    var restaurantImageData = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        //Part 6
+        let path = Bundle.main.path(forResource: "Property List", ofType: "plist")
+        let dict = NSDictionary(contentsOfFile: path!)
+        
+        restaurantImageData = dict!.object(forKey:"restaurantImage") as! [String]
+    
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    //Part 7
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath as IndexPath, animated: true)
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "mySegue"
+        {
+            let s1 = segue.destination as! detailViewController
+            let imageIndex = tableView.indexPathForSelectedRow?.row
+            s1.imagePass = restaurantImageData[imageIndex!]
+        }
+    }
+    
 }
 
---
+++ HackwichFive2/detailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  detailViewController.swift
+//  HackwichFive2
+//
+//  Created by Kevin Bechayda on 3/19/18.
+//  Copyright © 2018 Kevin Bechayda. All rights reserved.
+//
+
+import UIKit
+
+class detailViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    
+    var imagePass: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+   
+        if let imageName = imagePass {
+            imageView.image = UIImage(named: imageName)
+        }
+    
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
